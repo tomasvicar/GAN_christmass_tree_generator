3cba09c1373cf88d49244e15c8218afe7df78ff5
Vičar Tomáš (147472) <vicar@vutbr.cz>
2020-12-02 08:55:58 +0100

    wgan clipping is waorking (litle bit)

--- nets.py
+++ nets.py
@@ -11,36 +11,23 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            
             nn.ConvTranspose2d( nz, ngf * 16, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 16),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
-            nn.Conv2d(ngf * 16, ngf * 8, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(ngf * 8, ngf * 8, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            nn.Conv2d(ngf * 8, ngf * 4, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
-            nn.ConvTranspose2d( ngf * 4, ngf * 4, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d( ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.Conv2d(ngf * 4, ngf * 2, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
-            nn.ConvTranspose2d( ngf * 2, ngf * 2, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            nn.Conv2d(ngf * 2, ngf * 1, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(ngf * 1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d( ngf*1, ngf, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d( ngf*2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # state size. (nc) x 64 x 64
@@ -75,38 +62,22 @@ class Discriminator(nn.Module):
         self.main = nn.Sequential(
             
             # input is (nc) x 128 x 128
-            nn.Conv2d(nc, ndf, 3, 1, 1, bias=False),
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.BatchNorm2d(ndf),
-            nn.Conv2d(ndf, ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.BatchNorm2d(ndf),
             # input is (ndf) x 64 x 64
-            nn.Conv2d(ndf, ndf, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.BatchNorm2d(ndf),
             nn.Conv2d(ndf, ndf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf*2, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.BatchNorm2d(ndf*2),
-            nn.Conv2d(ndf*2, ndf * 2, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf*2, ndf* 4, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.BatchNorm2d(ndf*4),
-            nn.Conv2d(ndf * 4, ndf * 4, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf*4, ndf* 8, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.BatchNorm2d(ndf*8),
-            nn.Conv2d(ndf * 8, ndf * 8, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
@@ -126,4 +97,3 @@ class Discriminator(nn.Module):
         elif classname.find('BatchNorm') != -1:
             nn.init.normal_(m.weight.data, 1.0, 0.02)
             nn.init.constant_(m.bias.data, 0)
-    
